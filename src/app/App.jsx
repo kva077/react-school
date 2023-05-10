230077f0366cac9d21defe662250a60f82fbfed5
Колесников Влад <vkolesnikov@softrust.ru>
2023-05-10 16:02:36 +0300

    first commit

---
+++ src/app/App.jsx
@@ -0,0 +1,11 @@
+import Main from "./layouts/main";
+
+const App = () => {
+    return (
+        <>
+            <Main />
+        </>
+    );
+};
+
+export default App;
---
+++ src/app/layouts/login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Main from "./main";
+
+const Login = () => {
+    return (
+        <>
+            <h1>Login</h1>
+            <button component={Main}></button>
+        </>
+    );
+};
+
+export default Login;
